da9da7a237d979008a5e12f48919d746d095c605
visala123 <visala.k@algorims.com>
2025-04-30 13:08:13 +0800

    commiting to feature branch

---
+++ terraform/terraform.tf
@@ -0,0 +1,38 @@
+terraform {
+
+  # cloud {
+  #   workspaces {
+  #     name = "terraform-eks"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+    
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+backend "s3" {
+    bucket = "terraform-backend-remote-vpc-github-actions"
+    key    = "helloworld/terraform.tfstate"
+    region = "us-east-1"
+    }
